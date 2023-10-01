27bf246e983d24517fdb8bc0142b2e05b16f32ec
siddhantCodes <siddhantk232@gmail.com>
2023-10-02 00:52:58 +0530

    learning django

--- flake.nix
+++ flake.nix
@@ -1,24 +1,33 @@
 {
   description = "Python shell flake";
 
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        pythonEnv = pkgs.python3.withPackages(ps: with ps; [ openpyxl cairosvg ]);
-      in
-      {
+        django4 = with pkgs.python311Packages;
+          buildPythonPackage rec {
+            pname = "Django";
+            version = "4.2.5";
+            src = fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-XlwclUj/t3lrSopHgumi5aPfNhUln8G/0+vHO2RhRsE=";
+            };
+
+            propagatedBuildInputs = [ asgiref sqlparse ];
+            doCheck = false;
+          };
+        pythonEnv = pkgs.python311.withPackages
+          (ps: with ps; [ openpyxl cairosvg django4 ]);
+      in {
         devShells.default = pkgs.mkShellNoCC {
-          packages = [ pythonEnv pkgs.pdftk pkgs.black ];
+          packages = [ pythonEnv pkgs.pdftk pkgs.black pkgs.nixfmt ];
 
           shellHook = ''
             export PYTHONPATH="${pythonEnv}/bin/python"
           '';
         };
-      }
-    );
+      });
 }
