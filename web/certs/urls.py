19165e28c6d2795e59afd265ed4fcc3435d45ddc
siddhantCodes <siddhantk232@gmail.com>
2023-10-08 01:14:44 +0530

    upload pdf template

--- web/certs/urls.py
+++ web/certs/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "certs"
 urlpatterns = [
     # /polls/
+    path("fill/", views.fill, name="fill"),
+    path("create/", views.create, name="create"),
     path("", views.index, name="index"),
     # /polls/5/
     # path("<int:question_id>/", views.detail, name="detail"),
--- web/certs/views.py
+++ web/certs/views.py
@@ -1,7 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, reverse
+from django.http import HttpResponseBadRequest, HttpResponseRedirect, HttpResponse
+from django import forms
+from django.core.files.uploadedfile import UploadedFile
 
-# Create your views here.
+from web.settings import BASE_DIR
+
+TEMPLATE_URL = BASE_DIR / "template.pdf"
+
+
+class UploadFileForm(forms.Form):
+    pdf_template = forms.FileField(
+        label="PDF Template",
+        allow_empty_file=False,
+        required=True,
+        widget=forms.FileInput(attrs={"accept": ".pdf"}),
+    )
+
 
 def index(request):
-    return render(request, "certs/index.html")
+    upload_form = UploadFileForm()
+    return render(request, "certs/index.html", {"form": upload_form})
 
+
+def create(request):
+    if not request.method == "POST":
+        return redirect(reverse("certs:index"))
+
+    form = UploadFileForm(request.POST, request.FILES)
+
+    if not form.is_valid():
+        return HttpResponseBadRequest(b"Invalid form")
+
+    file: UploadedFile | None = form.cleaned_data.get("pdf_template")
+
+    assert file is not None
+
+    if not file.content_type == "application/pdf":
+        return HttpResponseBadRequest(b"Only pdf files are allowed")
+
+    with open(TEMPLATE_URL, "wb+") as f:
+        for chunk in file.chunks():
+            f.write(chunk)
+
+    return redirect(reverse("certs:fill"))
+
+
+def fill(request):
+    return render(request, "certs/fill.html", {"pdf_url": TEMPLATE_URL})
