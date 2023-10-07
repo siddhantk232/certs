ba37c82d586d9fdf241c85a29bdfa88f52c576d8
siddhantCodes <siddhantk232@gmail.com>
2023-10-07 22:59:07 +0530

    init certs django app

--- web/web/settings.py
+++ web/web/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
+    "certs.apps.CertsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
